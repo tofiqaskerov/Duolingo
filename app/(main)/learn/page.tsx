18470aab1f02a3a0b4b19e35b708e78b4b733fff
tofiqaskerov <tofiqaskerov71@gmail.com>
2024-07-16 22:08:20 +0400

    DB connections has been added

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -16,6 +16,7 @@ const LearnPage = () => {
        </StickyWrapper>
        <FeedWrapper>
             <Header title="Spanish"/>
+           
        </FeedWrapper>
     </div>
   )
--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 import Image from "next/image";
+import { InfinityIcon } from "lucide-react";
 
 type Props = {
-  activeCourse: {title:string, imageSrc:string};
+  activeCourse: { title: string; imageSrc: string };
   hearts: number;
   points: number;
   hasActiveSubscription: boolean;
@@ -18,7 +19,7 @@ export const UserProgress = ({
   return (
     <div className="flex flex-items-center justify-between gap-x-2 w-full">
       <Link href="/courses">
-        <Button>
+        <Button variant="ghost">
           <Image
             className="rounded-md border"
             src={activeCourse.imageSrc}
@@ -26,6 +27,34 @@ export const UserProgress = ({
             width={32}
             height={32}
           />
+        </Button>
+      </Link>
+      <Link href="/shop">
+        <Button variant="ghost" className="text-orange-500">
+          <Image
+            src="/points.svg"
+            alt="Points"
+            className="mr-2"
+            height={28}
+            width={28}
+          />
+          {points}
+        </Button>
+      </Link>
+      <Link href="/shop">
+        <Button variant="ghost" className="text-rose-500">
+          <Image
+            src="/heart.svg"
+            alt="Heart"
+            className="mr-2"
+            height={22}
+            width={22}
+          />
+          {hasActiveSubscription ? (
+            <InfinityIcon className="h-4 w-4 stroke-[3]" />
+          ) : (
+            hearts
+          )}
         </Button>
       </Link>
     </div>
