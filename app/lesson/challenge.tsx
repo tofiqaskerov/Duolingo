0bd8d93ff99fa6a857ea5c3037fbb63aace69dd7
tofiqaskerov <tofiqaskerov71@gmail.com>
2024-08-18 00:55:22 +0400

    Challenge Options Card and AI Voices has been added

--- app/lesson/challenge.tsx
+++ app/lesson/challenge.tsx
@@ -1,4 +1,6 @@
 import { challengeOptions, challenges } from "@/db/schema";
+import { cn } from "@/lib/utils";
+import { Card } from "./card";
 
 type Props = {
   options: (typeof challengeOptions.$inferSelect)[];
@@ -17,5 +19,29 @@ export const Challenge = ({
   disabled,
   type,
 }: Props) => {
-  return <div>Challenge</div>;
+  return( 
+  <div className={cn(
+    "grid gap-2",
+    type === "ASSIST" && "grid-cols-1",
+    type === "SELECT" && "grid-cols-2 lg:grid-cols-[repeat(auto-fit,minmax(0,1fr))]"
+  )}>
+    {
+        options.map((option, i) => (
+            <Card
+                key={option.id}
+                id={option.id}
+                text={option.text}
+                imageSrc={option.imageSrc}
+                shortcut={`${i + 1}`}
+                selected={selectedOption === option.id}
+                onClick={() =>onSelect(option.id)}
+                status={status}
+                audioSrc={option.audioSrc}
+                disabled={disabled}
+                type={type}
+            />
+        ))
+    }
+  </div>
+ );
 };
